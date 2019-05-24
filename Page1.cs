846c713938b1f51c7731d00bab651d316cd5784b
astn <austin.w.harris@gmail.com>
2019-05-24 15:01:35 -0600

    initial

---
+++ Page1.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace testproject
+{
+    public static class Page1
+    {
+        // Returns "Hello World!"
+        public static string HelloWorld()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Return the sum of two numbers
+        public static int sum(int a, int b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Return the product of two numbers
+        public static int product(int a, int b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Return the square of a number
+        public static int square(int a)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // return the cube of a number
+        public static int cube(int a)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // is 'a' a perfect square?
+        public static bool isPerfectSquare(int a)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // return the quotient of a fraction
+        public static int quotient(int a, int b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // return the int remainder of a fraction
+        public static int remainder(int a, int b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // square all the xs
+        public static List<int> squareAll(IEnumerable<int> xs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // cube all the xs
+        public static List<int> cubeAll(IEnumerable<int> xs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // sum all the xs
+        public static int sumAll(IEnumerable<int> xs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // reverse xs
+        public static List<int> reverse(IEnumerable<int> xs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // produce an infinite sequence of integers starting with x
+        public static IEnumerable<int> countFrom(int x)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // produce the squares of an infinite sequence starting with x
+        public static IEnumerable<int> squaresFrom(int x)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // produce the product of every x with it's y in the xs and ys
+        public static IEnumerable<int> productOfStreams(IEnumerable<int> xs, IEnumerable<int> ys)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
