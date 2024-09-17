08e5d6fc999e51e9d5c6d8d3126ae47bc2c3e984
Astn <austin.w.harris@gmail.com>
2024-09-17 14:30:18 -0600

    Improved comments

--- CodeEval1.cs
+++ CodeEval1.cs
@@ -11,11 +11,15 @@ namespace testproject
     {
         /// <summary>
         /// Generates an infinite sequence of Fibonacci numbers starting from specified initial values.
-        /// If the initial values for f1 and f2 are (0, 1) then the sequence will start with [0, 1, 1, 2, 3, 5, 8, 13, ...].
+        /// If the initial values for <paramref name="f1"/> and <paramref name="f2"/> are (0, 1), then the sequence will start with [0, 1, 1, 2, 3, 5, 8, 13, ...].
         /// </summary>
         /// <param name="f1">The first term in the Fibonacci sequence.</param>
         /// <param name="f2">The second term in the Fibonacci sequence.</param>
         /// <returns>An infinite sequence of Fibonacci numbers as <see cref="IEnumerable{int}"/>.</returns>
+        /// <remarks>
+        /// <para><b>Time Complexity:</b> O(1) per generated number.</para>
+        /// <para><b>Memory Complexity:</b> O(1) extra space.</para>
+        /// </remarks>
         public static IEnumerable<int> allFib(int f1, int f2)
         {
             throw new NotImplementedException();
@@ -26,6 +30,9 @@ namespace testproject
         /// </summary>
         /// <param name="n">The number of Fibonacci numbers to generate. Must be non-negative.</param>
         /// <returns>A sequence of the first N Fibonacci numbers as <see cref="IEnumerable{int}"/>.</returns>
+        /// <remarks>
+        /// <para><b>Time Complexity:</b> O(n).</para>
+        /// <para><b>Memory Complexity:</b> O(n).</para>
         public static IEnumerable<int> firstNFibonacciNumbers(int n)
         {
             throw new NotImplementedException();
@@ -36,6 +43,10 @@ namespace testproject
         /// </summary>
         /// <param name="n">The number to check.</param>
         /// <returns>True if <paramref name="n"/> is a Fibonacci number, otherwise false.</returns>
+        /// <remarks>
+        /// <para><b>Time Complexity:</b> O(1).</para>
+        /// <para><b>Memory Complexity:</b> O(1).</para>
+        /// </remarks>
         public static bool isFibNumber(int n)
         {
             throw new NotImplementedException();
@@ -47,6 +58,10 @@ namespace testproject
         /// <param name="lower">The lower bound of the range.</param>
         /// <param name="upper">The upper bound of the range.</param>
         /// <returns>The sum of all Fibonacci numbers between <paramref name="lower"/> and <paramref name="upper"/>, inclusive.</returns>
+        /// <remarks>
+        /// <para><b>Time Complexity:</b> O(log upper), since the number of Fibonacci numbers less than <paramref name="upper"/> grows logarithmically.</para>
+        /// <para><b>Memory Complexity:</b> O(1).</para>
+        /// </remarks>
         public static int sumSomeFib(int lower, int upper)
         {
             throw new NotImplementedException();
@@ -57,6 +72,10 @@ namespace testproject
         /// </summary>
         /// <param name="xs">The sequence of integers to sum.</param>
         /// <returns>The sum of all integers in <paramref name="xs"/>.</returns>
+        /// <remarks>
+        /// <para><b>Time Complexity:</b> O(n) total work; actual wall-clock time is reduced through parallel execution, depending on available processors.</para>
+        /// <para><b>Memory Complexity:</b> O(log n), due to the call stack in parallel reduction.</para>
+        /// </remarks>
         public static int parallelSum(IEnumerable<int> xs)
         {
             throw new NotImplementedException();
@@ -69,7 +88,9 @@ namespace testproject
         /// <param name="substring">The substring to search for within the stream.</param>
         /// <returns>True if <paramref name="substring"/> is found within the stream, otherwise false.</returns>
         /// <remarks>
-        /// This function is designed to be memory efficient and handle cases where the substring spans multiple chunks.
+        /// <para>This function is designed to be memory efficient and handle cases where the substring spans multiple chunks.</para>
+        /// <para><b>Time Complexity:</b> O(N), where N is the total length of the text in the chunks.</para>
+        /// <para><b>Memory Complexity:</b> O(L), where L is the length of the substring.</para>
         /// </remarks>
        
         public static bool isTextInStream(IEnumerable<FileChunk> chunksFromA10GBfile, string substring)
@@ -83,7 +104,9 @@ namespace testproject
         /// <param name="xs">The sequence of integers to filter.</param>
         /// <returns>A filtered sequence as <see cref="IEnumerable{int}"/> according to the specified criteria.</returns>
         /// <remarks>
-        /// This method is memory efficient and does not require converting the entire sequence into a List or Array.
+        /// <para>This method is memory efficient and does not require converting the entire sequence into a List or Array.</para>
+        /// <para><b>Time Complexity:</b> O(n), where n is the number of elements in <paramref name="xs"/>.</para>
+        /// <para><b>Memory Complexity:</b> O(1).</para>
         /// </remarks>
         public static IEnumerable<int> FilterOutNegativeExceptFirst(IEnumerable<int> xs)
         {
--- Warmup.cs
+++ Warmup.cs
@@ -10,6 +10,9 @@ namespace testproject
         /// Returns the greeting message "Hello World!".
         /// </summary>
         /// <returns>A string containing "Hello World!".</returns>
+        /// <remarks>
+        /// <para><b>Example:</b> The method should simply return the string "Hello World!".</para>
+        /// </remarks>
         public static string HelloWorld()
         {
             throw new NotImplementedException();
@@ -18,9 +21,13 @@ namespace testproject
         /// <summary>
         /// Calculates the sum of two integers.
         /// </summary>
-        /// <param name="a">The first integer.</param>
-        /// <param name="b">The second integer.</param>
-        /// <returns>The sum of <paramref name="a"/> and <paramref name="b"/>.</returns>
+        /// <param name="a">The first integer, denoted as <c>a</c>.</param>
+        /// <param name="b">The second integer, denoted as <c>b</c>.</param>
+        /// <returns>The sum of <paramref name="a"/> and <paramref name="b"/>, calculated as <c>a + b</c>.</returns>
+        /// <remarks>
+        /// <para><b>Mathematical Definition:</b> <c>sum(a, b) = a + b</c>.</para>
+        /// <para><b>Example:</b> If <c>a = 2</c> and <c>b = 3</c>, then <c>sum(a, b) = 5</c>.</para>
+        /// </remarks>
         public static int sum(int a, int b)
         {
             throw new NotImplementedException();
@@ -29,9 +36,13 @@ namespace testproject
         /// <summary>
         /// Calculates the product of two integers.
         /// </summary>
-        /// <param name="a">The first integer.</param>
-        /// <param name="b">The second integer.</param>
-        /// <returns>The product of <paramref name="a"/> and <paramref name="b"/>.</returns>
+        /// <param name="a">The first integer, denoted as <c>a</c>.</param>
+        /// <param name="b">The second integer, denoted as <c>b</c>.</param>
+        /// <returns>The product of <paramref name="a"/> and <paramref name="b"/>, calculated as <c>a × b</c>.</returns>
+        /// <remarks>
+        /// <para><b>Mathematical Definition:</b> <c>product(a, b) = a × b</c>.</para>
+        /// <para><b>Example:</b> If <c>a = 4</c> and <c>b = 5</c>, then <c>product(a, b) = 20</c>.</para>
+        /// </remarks>
         public static int product(int a, int b)
         {
             throw new NotImplementedException();
@@ -40,8 +51,12 @@ namespace testproject
         /// <summary>
         /// Returns the square of an integer.
         /// </summary>
-        /// <param name="a">The integer to square.</param>
-        /// <returns>The square of <paramref name="a"/>.</returns>
+        /// <param name="a">The integer to square, denoted as <c>a</c>.</param>
+        /// <returns>The square of <paramref name="a"/>, calculated as <c>a²</c>.</returns>
+        /// <remarks>
+        /// <para><b>Mathematical Definition:</b> <c>square(a) = a²</c>.</para>
+        /// <para><b>Example:</b> If <c>a = 3</c>, then <c>square(a) = 9</c>.</para>
+        /// </remarks>
         public static int square(int a)
         {
             throw new NotImplementedException();
@@ -50,20 +65,28 @@ namespace testproject
         /// <summary>
         /// Returns the cube of an integer.
         /// </summary>
-        /// <param name="a">The integer to cube.</param>
-        /// <returns>The cube of <paramref name="a"/>.</returns>
+        /// <param name="a">The integer to cube, denoted as <c>a</c>.</param>
+        /// <returns>The cube of <paramref name="a"/>, calculated as <c>a³</c>.</returns>
+        /// <remarks>
+        /// <para><b>Mathematical Definition:</b> <c>cube(a) = a³</c>.</para>
+        /// <para><b>Example:</b> If <c>a = 2</c>, then <c>cube(a) = 8</c>.</para>
+        /// </remarks>
         public static int cube(int a)
         {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// Calculates the quotient of dividing two integers.
+        /// Calculates the quotient of dividing two integers using integer division.
         /// </summary>
-        /// <param name="a">The dividend.</param>
-        /// <param name="b">The divisor. Must not be 0.</param>
-        /// <returns>The quotient of <paramref name="a"/> divided by <paramref name="b"/>.</returns>
+        /// <param name="a">The dividend, denoted as <c>a</c>.</param>
+        /// <param name="b">The divisor, denoted as <c>b</c>. Must not be 0.</param>
+        /// <returns>The quotient of <paramref name="a"/> divided by <paramref name="b"/>, calculated as <c>a ÷ b</c> using integer division.</returns>
         /// <exception cref="DivideByZeroException">Thrown when <paramref name="b"/> is 0.</exception>
+        /// <remarks>
+        /// <para><b>Mathematical Definition:</b> <c>quotient(a, b) = a ÷ b</c> (integer division).</para>
+        /// <para><b>Example:</b> If <c>a = 7</c> and <c>b = 2</c>, then <c>quotient(a, b) = 3</c>.</para>
+        /// </remarks>
         public static int quotient(int a, int b)
         {
             throw new NotImplementedException();
@@ -72,30 +95,41 @@ namespace testproject
         /// <summary>
         /// Calculates the remainder of dividing two integers.
         /// </summary>
-        /// <param name="a">The dividend.</param>
-        /// <param name="b">The divisor. Must not be 0.</param>
-        /// <returns>The remainder of <paramref name="a"/> divided by <paramref name="b"/>.</returns>
+        /// <param name="a">The dividend, denoted as <c>a</c>.</param>
+        /// <param name="b">The divisor, denoted as <c>b</c>. Must not be 0.</param>
+        /// <returns>The remainder of <paramref name="a"/> divided by <paramref name="b"/>, calculated as <c>a mod b</c>.</returns>
         /// <exception cref="DivideByZeroException">Thrown when <paramref name="b"/> is 0.</exception>
+        /// <remarks>
+        /// <para><b>Mathematical Definition:</b> <c>remainder(a, b) = a mod b</c>.</para>
+        /// <para><b>Example:</b> If <c>a = 7</c> and <c>b = 2</c>, then <c>remainder(a, b) = 1</c>.</para>
+        /// </remarks>
         public static int remainder(int a, int b)
         {
             throw new NotImplementedException();
         }
-        
+
         /// <summary>
         /// Determines whether an integer is a perfect square.
         /// </summary>
-        /// <param name="a">The integer to check.</param>
-        /// <returns>True if <paramref name="a"/> is a perfect square, otherwise false.</returns>
+        /// <param name="a">The integer to check, denoted as <c>a</c>.</param>
+        /// <returns><c>true</c> if <paramref name="a"/> is a perfect square (i.e., there exists an integer <c>n</c> such that <c>n² = a</c>), otherwise <c>false</c>.</returns>
+        /// <remarks>
+        /// <para><b>Example:</b> <c>isPerfectSquare(16)</c> returns <c>true</c> because <c>4² = 16</c>.</para>
+        /// <para><b>Example:</b> <c>isPerfectSquare(15)</c> returns <c>false</c>.</para>
+        /// </remarks>
         public static bool isPerfectSquare(int a)
         {
             throw new NotImplementedException();
         }
-        
+
         /// <summary>
         /// Squares each number in a collection.
         /// </summary>
-        /// <param name="xs">The collection of integers to square.</param>
-        /// <returns>A new list containing the squares of each number in <paramref name="xs"/>.</returns>
+        /// <param name="xs">The collection of integers to square, denoted as <c>{ x₁, x₂, ..., xₙ }</c>.</param>
+        /// <returns>A new list containing the squares of each number in <paramref name="xs"/>, i.e., <c>{ x₁², x₂², ..., xₙ² }</c>.</returns>
+        /// <remarks>
+        /// <para><b>Example:</b> If <c>xs = [1, 2, 3]</c>, then <c>squareAll(xs) = [1, 4, 9]</c>.</para>
+        /// </remarks>
         public static List<int> squareAll(IEnumerable<int> xs)
         {
             throw new NotImplementedException();
@@ -104,8 +138,11 @@ namespace testproject
         /// <summary>
         /// Cubes each number in a collection.
         /// </summary>
-        /// <param name="xs">The collection of integers to cube.</param>
-        /// <returns>A new list containing the cubes of each number in <paramref name="xs"/>.</returns>
+        /// <param name="xs">The collection of integers to cube, denoted as <c>{ x₁, x₂, ..., xₙ }</c>.</param>
+        /// <returns>A new list containing the cubes of each number in <paramref name="xs"/>, i.e., <c>{ x₁³, x₂³, ..., xₙ³ }</c>.</returns>
+        /// <remarks>
+        /// <para><b>Example:</b> If <c>xs = [1, 2, 3]</c>, then <c>cubeAll(xs) = [1, 8, 27]</c>.</para>
+        /// </remarks>
         public static List<int> cubeAll(IEnumerable<int> xs)
         {
             throw new NotImplementedException();
@@ -114,8 +151,11 @@ namespace testproject
         /// <summary>
         /// Sums all numbers in a collection.
         /// </summary>
-        /// <param name="xs">The collection of integers to sum.</param>
-        /// <returns>The sum of all numbers in <paramref name="xs"/>.</returns>
+        /// <param name="xs">The collection of integers to sum, denoted as <c>{ x₁, x₂, ..., xₙ }</c>.</param>
+        /// <returns>The sum of all numbers in <paramref name="xs"/>, calculated as <c>∑(xᵢ)</c> for <c>i = 1</c> to <c>n</c>.</returns>
+        /// <remarks>
+        /// <para><b>Example:</b> If <c>xs = [1, 2, 3, 4]</c>, then <c>sumAll(xs) = 10</c>.</para>
+        /// </remarks>
         public static int sumAll(IEnumerable<int> xs)
         {
             throw new NotImplementedException();
@@ -124,8 +164,11 @@ namespace testproject
         /// <summary>
         /// Reverses the order of numbers in a collection.
         /// </summary>
-        /// <param name="xs">The collection of integers to reverse.</param>
-        /// <returns>A new list containing all numbers in <paramref name="xs"/> in reverse order.</returns>
+        /// <param name="xs">The collection of integers to reverse, denoted as <c>[x₁, x₂, ..., xₙ]</c>.</param>
+        /// <returns>A new list containing all numbers in <paramref name="xs"/> in reverse order, i.e., <c>[xₙ, xₙ₋₁, ..., x₁]</c>.</returns>
+        /// <remarks>
+        /// <para><b>Example:</b> If <c>xs = [1, 2, 3]</c>, then <c>reverse(xs) = [3, 2, 1]</c>.</para>
+        /// </remarks>
         public static List<int> reverse(IEnumerable<int> xs)
         {
             throw new NotImplementedException();
@@ -134,8 +177,11 @@ namespace testproject
         /// <summary>
         /// Produces an infinite sequence of integers starting from a specified number.
         /// </summary>
-        /// <param name="x">The starting number of the sequence.</param>
-        /// <returns>An infinite sequence of integers starting at <paramref name="x"/>.</returns>
+        /// <param name="x">The starting number of the sequence, denoted as <c>x</c>.</param>
+        /// <returns>An infinite sequence of integers starting at <paramref name="x"/>, i.e., <c>[x, x + 1, x + 2, ...]</c>.</returns>
+        /// <remarks>
+        /// <para><b>Example:</b> If <c>x = 5</c>, the sequence is <c>[5, 6, 7, 8, ...]</c>.</para>
+        /// </remarks>
         public static IEnumerable<int> countFrom(int x)
         {
             throw new NotImplementedException();
@@ -144,8 +190,11 @@ namespace testproject
         /// <summary>
         /// Produces an infinite sequence of squares starting from the square of a specified number.
         /// </summary>
-        /// <param name="x">The number from which to start the sequence of squares.</param>
-        /// <returns>An infinite sequence of squares starting at the square of <paramref name="x"/>.</returns>
+        /// <param name="x">The number from which to start the sequence of squares, denoted as <c>x</c>.</param>
+        /// <returns>An infinite sequence of squares starting at <c>x²</c>, i.e., <c>[x², (x + 1)², (x + 2)², ...]</c>.</returns>
+        /// <remarks>
+        /// <para><b>Example:</b> If <c>x = 2</c>, the sequence is <c>[4, 9, 16, 25, ...]</c>.</para>
+        /// </remarks>
         public static IEnumerable<int> squaresFrom(int x)
         {
             throw new NotImplementedException();
@@ -154,9 +203,14 @@ namespace testproject
         /// <summary>
         /// Calculates the product of corresponding elements in two collections.
         /// </summary>
-        /// <param name="xs">The first collection of integers.</param>
-        /// <param name="ys">The second collection of integers.</param>
-        /// <returns>A new sequence containing the products of corresponding elements from <paramref name="xs"/> and <paramref name="ys"/>.</returns>
+        /// <param name="xs">The first collection of integers, denoted as <c>{ x₁, x₂, ..., xₙ }</c>.</param>
+        /// <param name="ys">The second collection of integers, denoted as <c>{ y₁, y₂, ..., yₙ }</c>.</param>
+        /// <returns>A new sequence containing the products of corresponding elements from <paramref name="xs"/> and <paramref name="ys"/>, i.e., <c>{ x₁ × y₁, x₂ × y₂, ..., xₙ × yₙ }</c>.</returns>
+        /// <remarks>
+        /// <para><b>Mathematical Definition:</b> <c>productOfStreams(xs, ys) = { xᵢ × yᵢ }</c> for <c>i = 1</c> to <c>n</c>.</para>
+        /// <para><b>Example:</b> If <c>xs = [1, 2, 3]</c> and <c>ys = [4, 5, 6]</c>, then <c>productOfStreams(xs, ys) = [4, 10, 18]</c>.</para>
+        /// <para>The sequences are assumed to be of the same length. If they are of different lengths, the resulting sequence will be as long as the shorter one.</para>
+        /// </remarks>
         public static IEnumerable<int> productOfStreams(IEnumerable<int> xs, IEnumerable<int> ys)
         {
             throw new NotImplementedException();
